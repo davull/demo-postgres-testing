937411ed13a470522b41241fe281ad5e399d15cb
David Ullrich <david.ullrich@ullrich-software.de>
2024-10-30 19:00:04 +0100

    Add env test

---
+++ src/Demo.Lib.Tests/EnvironmentTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Demo.Lib.Tests;
+
+public class EnvironmentTests
+{
+    [Test]
+    public void Platform_Should_BeWindowsOrUnix()
+    {
+        var actual = Environment.OSVersion.Platform;
+        TestContext.WriteLine($"Platform: {actual}");
+
+        PlatformID[] expected = [PlatformID.Win32NT, PlatformID.Unix];
+
+        expected.Should().Contain(actual);
+    }
+}
